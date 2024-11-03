1c21fc55868360ae8f8df31cf3384715a1b94e87
Raph Marquez <m.raphael150@gmail.com>
2024-11-03 01:06:29 -0600

    testing scripts for each environment

---
+++ tests/test_core_env.py
@@ -0,0 +1,8 @@
+# testing installed packages in core
+import pandas as pd
+import sqlalchemy
+import dbt #  ensure dbt is available
+import psycopg2 # for psotgresql
+import airflow # for Airflow
+
+print("Core environment packages loaded successfully")
